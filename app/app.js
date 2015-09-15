7f0030118b90654bd067847c83cecf22570554f4
Franky Chanyau <franky@movingvillage.com>
2015-09-15 16:13:43 +1200

    cleanup

--- app/app.js
+++ app/app.js
@@ -1,16 +1,7 @@
 t.app = new t.libraries.App({
     title: "My App",
     subTitle: "Welcome",
-    constructor: function(){
-        this.statusBar();
-    },
     ready: function(){
         this.goTo("default");
-    },
-    statusBar: function(){
-        if(this.isCordova){
-            StatusBar.overlaysWebView(true);
-            StatusBar.styleLightContent();
-        }
     }
 });
